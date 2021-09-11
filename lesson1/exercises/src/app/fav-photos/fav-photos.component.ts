949b8ef65fa5276f4d142b091b323dd4cbaf7626
PeachPaulison <84045459+PeachPaulison@users.noreply.github.com>
2021-09-11 12:02:57 -0400

    Part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Photos of the Day';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://i.etsystatic.com/15257668/r/il/dbe591/2268357766/il_794xN.2268357766_5mxf.jpg';
+  image3 = 'https://i.etsystatic.com/13144415/r/il/1cd8ce/2243805306/il_794xN.2243805306_9d2m.jpg';
 
   constructor() { }
 
